58e3e17069196a085803379c7c7a0cb6386478d1
thot-experiment <thot@thiic.cc>
2023-06-21 16:50:00 -0700

    reworked text splitting with more magic numbers

--- main.mjs
+++ main.mjs
@@ -8,40 +8,44 @@ const addTextToImage = (imageUrl, text) => {
   const draw_wrapped_text = (context, text, color, box) => {
     let [ textBoxX , textBoxY,textBoxWidth , textBoxHeight] = box
     textBoxX += textBoxWidth/2
-    const font_size = (textBoxWidth*2)/text.length
 
+    let lines = ((text, ratio) => {
+      let tl = text.length
+      let tll = ((tl/(ratio*0.62))/tl)**1.4*tl+3
+      console.log(tll)
+      let lines = ['']
+      let pos = 0
+      text.split(/\W/).forEach(word => {
+        if (lines[pos].length+word.length < tll) {
+          lines[pos] += ' '+word
+        } else {
+          pos++ 
+          lines[pos] = word
+        }
+      })
+      return lines
+    })(text,3).filter(a => a)
+
+    let maxl = lines.reduce((c, ll) => ll.length + c,0)/lines.length
+    //let maxl = lines.reduce((c, ll) => ll.length < c?c:ll.length,0)
+
+    const font_size = textBoxWidth/maxl*1.2
+    const lineHeight = font_size*1.3
+
+    console.log(lines,maxl)
     context.font = `bold italic ${font_size}pt Arial`
     //context.fillRect(textBoxX, textBoxY, textBoxWidth, textBoxHeight)
     context.fillStyle = color
     context.textBaseline = 'top'
     context.textAlign = 'center'
-    
-    const words = text.split(' ')
-    let line = ''
-    let lineNumber = 0
-    const lineHeight = font_size*1.3
-    
-    for (let i = 0; i < words.length; i++) {
-      const testLine = line + words[i] + ' '
-      const metrics = context.measureText(testLine)
-      const testWidth = metrics.width
-      
-      if (testWidth > textBoxWidth && i > 0) {
-        context.fillText(line, textBoxX, textBoxY + (lineNumber * lineHeight))
-        line = words[i] + ' '
-        lineNumber++
-      } else {
-        line = testLine
-      }
-    }
-    
-    context.fillText(line, textBoxX, textBoxY + (lineNumber * lineHeight))
+
+    lines.forEach((line,i) => context.fillText(line, textBoxX, textBoxY + (i * lineHeight)))
   }
-  
+
   image.onload = () => {
     canvas.width = image.width
     canvas.height = image.height
-    
+
     context.drawImage(image, 0, 0)
     draw_wrapped_text(context, text, 'black', [23,86,156,60])
     draw_wrapped_text(context, text, 'black', [593,91,156,60])
@@ -49,7 +53,7 @@ const addTextToImage = (imageUrl, text) => {
     draw_wrapped_text(context, text, 'black', [185,287,135,55])
     draw_wrapped_text(context, text, 'black', [75,415,135,55])
     draw_wrapped_text(context, text, 'white', [593,294,156,44])
-    
+
     const modImage = new Image()
     modImage.src = canvas.toDataURL()
     document.body.appendChild(modImage)
